4ff6733d8f6648298a4487f92ce77a071668e0d8
Sofia <sofia-tovedal@gmail.com>
2020-03-07 17:17:39 +0100

    moving computoes

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { Client } = require('pg')
-
-const db_client = new Client({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'todos',
-	password: 'softov',
-	port: 5432,
-  })
 
 app.use(cors())
 
 const PORT = 3001
-db_client.connect()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/todos', (req, res) => {
-	db_client.query('SELECT * FROM lists', (db_err, db_res) => {
-		if(db_err){
-			res.send(db_err)
-		} else {
-			res.send(db_res)
-		}
+app.get('/lists', (req, res) => {
 
-	} )
 })
 
-app.post('/todos', (req, res) => {
-	console.log(req)
-	res.send("Thanks")
+app.post('/lists', (req, res) => {
+
 })
 
 app.get('/lists', (req, res) => res.send('Hello World!'))
---
+++ frontend/src/api/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const url = "http://localhost:3001/"
+
+const paths = {
+	todos: "todos/",
+	lists: "lists/"
+}
+
+const getLists() => {
+	axios.get('')
+
+}
